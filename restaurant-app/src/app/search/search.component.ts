c31edff8c5de9efb4d1db386b10df0f5f6e1bd50
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-10-09 18:10:18 +0400

    add: added the serach bar functionality with two way binding

--- restaurant-app/src/app/search/search.component.ts
+++ restaurant-app/src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-search',
@@ -8,12 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchComponent {
    searchTerm:String = "";
-   constructor(private route:ActivatedRoute) {}
+   constructor(private route:ActivatedRoute, private router:Router) {}
    ngOnInit(): void {
     this.route.params.subscribe(params => {
       if(params['searchTerm']){
        this.searchTerm = params['searchTerm'];
       }
     })
+   }
+
+   search(): void{
+     if(this.searchTerm){
+      this.router.navigateByUrl('/search/' + this.searchTerm);
+     }
    }
 }
