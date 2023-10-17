fde087985507cda71ea33dedb7e4c271312b3b4b
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-10-17 18:07:15 +0400

    create: cartItem Model & Cart Model

---
+++ restaurant-app/src/app/shared/models/Cart.ts
@@ -0,0 +1,15 @@
+import { cartItem } from "./CartItem";
+
+export class Cart{
+    items:cartItem[] = [];
+
+    get totalPrice(): number{
+        let totalPrice = 0;
+
+        this.items.forEach(item => {
+            totalPrice += item.price;
+        })
+
+        return totalPrice;
+    }
+}
