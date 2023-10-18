a13eb31400a1f5550a849ccfc1ce364d15884b74
Irakli <112602982+IkaMastera@users.noreply.github.com>
2023-10-18 16:56:38 +0400

    style: finished cart-page styles

--- restaurant-app/src/app/cart-page/cart-page.component.ts
+++ restaurant-app/src/app/cart-page/cart-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart/cart.service';
 import { Cart } from '../shared/models/Cart';
 import { CartItem } from '../shared/models/CartItem';
-import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -11,11 +10,7 @@ import { FoodService } from '../services/food/food.service';
 })
 export class CartPageComponent implements OnInit {
   cart!:Cart;
-  constructor(private cartService: CartService, private foodService:FoodService) {
-    let foods = foodService.getAll();
-    cartService.addToCart(foods[1]);
-    cartService.addToCart(foods[2]);
-    cartService.addToCart(foods[3]);
+  constructor(private cartService: CartService) {
     this.setCart();
   }
 
